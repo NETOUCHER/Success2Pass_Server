e3e46b1279283921c79028349b4ec9d3347dc932
Milkey Tan <819774989@qq.com>
2019-06-27 15:33:43 +0800

    addr+https

--- start.py
+++ start.py
@@ -2,7 +2,15 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from s2p import app
+
+https = False
 print('Success2Pass Server Running')
+
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5555)
+if https:
+    http_server = HTTPServer(WSGIContainer(app),ssl_options={
+           "certfile": os.path.join(os.path.abspath("."), "server.crt"),
+           "keyfile": os.path.join(os.path.abspath("."), "server.key")
+    })
+http_server.listen(5555, address="0.0.0.0")
 IOLoop.instance().start()
